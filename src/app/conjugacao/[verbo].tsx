8b7568c696a4d597aa460eca7958f99b0c05b7a8
Luiz Paulo de Lima Araújo <pluiz0674@gmail.com>
2024-08-08 11:47:10 -0300

    feat(app/verbo): commit emergencial

---
+++ src/app/conjugacao/[verbo].tsx
@@ -0,0 +1,79 @@
+import { View, Text, ScrollView } from "react-native";
+import { useLocalSearchParams, useGlobalSearchParams, Link } from "expo-router";
+
+import {
+  Accordion,
+  AccordionItem,
+  AccordionHeader,
+  AccordionContent,
+  AccordionTrigger,
+  AccordionTitleText,
+  AccordionIcon
+} from "@/components/ui/accordion";
+import {
+  Checkbox,
+  CheckboxIndicator,
+  CheckboxIcon,
+  CheckboxLabel
+} from "@/components/ui/checkbox";
+
+
+/** Seletores que configuram o exercicio de conjugação
+ *  a ser feito. Rota lida: /conjugacao/[VERBO]
+ *  */
+export default function RotaConjugacao()
+{
+
+  const glob  = useGlobalSearchParams();
+  const local = useLocalSearchParams();
+
+  console.log("Local:", local.verbo, "Global:", glob.verbo);
+
+  return (
+    <View>
+      <View id="tituloVerbo">
+        <Text>{ local.verbo }</Text>
+      </View>
+      <Text> AEDIFICA TUA VIA: </Text>
+        <Accordion
+          size="md"
+          variant="filled"
+          type="multiple"
+          isCollapsible={true}
+          isDisabled={false}
+          className="modoIndicativoVozAtiva"
+        >
+          <AccordionItem value="demonstracao">
+
+            <AccordionHeader>
+              <AccordionTrigger>
+                {({ isExpanded }) => {
+                  return (
+                    <>
+                      <AccordionTitleText>How do I place an order?</AccordionTitleText>
+                      {isExpanded ? (
+                        <Text>-</Text>
+                      ) : (
+                        <Text>+</Text>
+                      )}
+                    </>
+                  )
+                }}
+              </AccordionTrigger>
+            </AccordionHeader>
+
+            <AccordionContent>
+
+              <Checkbox value="demo" size="md" isInvalid={false} isDisabled={false}>
+                <CheckboxIndicator>
+                </CheckboxIndicator>
+                <CheckboxLabel> presente </CheckboxLabel>
+              </Checkbox>
+
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+    </View>
+  );
+}
+
