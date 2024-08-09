9fc334c1eaf3038b3d3464d3247c26863c435435
LPLA-br <pluiz0674@gmail.com>
2024-08-09 09:01:53 -0300

    feat (app/conjugacao): conclui implementacao da tela seletora
    Conclui a tela responsavel por configurar as etapas de exercicios
    de conjugacao de verbos. nao implementa logica por detras. problema
    dos icones dos checkboxes permanece
    
    tarefa B2

--- src/app/conjugacao/[verbo].tsx
+++ src/app/conjugacao/[verbo].tsx
@@ -1,17 +1,16 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
 import { useLocalSearchParams, useGlobalSearchParams, Link } from "expo-router";
+import { Stack } from "expo-router";
 
 import {
   Accordion,
   AccordionItem,
-  AccordionHeader,
   AccordionContent,
-  AccordionTrigger,
-  AccordionTitleText,
-  AccordionIcon
 } from "@/components/ui/accordion";
 
 import CaixaSeletora from "../../components/CaixaSeletora";
+import AcordeaoCabecalho from "@/src/components/AcordeaoCabecalho";
+import BotaoApp from "@/src/components/BotaoApp";
 
 /** Seletores que configuram o exercicio de conjugação
  *  a ser feito. Rota lida: /conjugacao/[VERBO]
@@ -25,51 +24,65 @@ export default function RotaConjugacao()
   console.log("Local:", local.verbo, "Global:", glob.verbo);
 
   return (
-    <View>
-      <View id="tituloVerbo">
-        <Text>{ local.verbo }</Text>
-      </View>
-      <Text> AEDIFICA TUA VIA: </Text>
-        <Accordion
-          size="md"
-          variant="filled"
-          type="multiple"
-          isCollapsible={true}
-          isDisabled={false}
-          className="modoIndicativoVozAtiva"
-        >
-          <AccordionItem value="demonstracao">
-
-            <AccordionHeader>
-              <AccordionTrigger>
-                {({ isExpanded }) => {
-                  return (
-                    <>
-                      <AccordionTitleText>How do I place an order?</AccordionTitleText>
-                      {isExpanded ? (
-                        <Text>-</Text>
-                      ) : (
-                        <Text>+</Text>
-                      )}
-                    </>
-                  )
-                }}
-              </AccordionTrigger>
-            </AccordionHeader>
-
-            <AccordionContent>
-
-            <CaixaSeletora titulo="presente" />
-            <CaixaSeletora titulo="imperfeito" />
-            <CaixaSeletora titulo="perfeito" />
-            <CaixaSeletora titulo="mais que perfeito" />
-            <CaixaSeletora titulo="futuro perfeito" />
-            <CaixaSeletora titulo="futuro" />
-
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
-    </View>
+    <SafeAreaView>
+      <ScrollView>
+        <View>
+          <Stack.Screen options={{ title: "CONIUNGERE" }}/>
+          <View id="tituloVerbo">
+            <Text>{ local.verbo }</Text>
+          </View>
+          <Text> AEDIFICA TUA VIA: </Text>
+            <Accordion
+              size="md"
+              variant="filled"
+              type="single"
+              isCollapsible={true}
+              isDisabled={false}
+              className="modoIndicativoVozAtiva"
+            >
+              <AccordionItem value="miva">
+                <AcordeaoCabecalho titulo="modo indicativo voz ativa" />
+                <AccordionContent>
+                  <CaixaSeletora titulo="presente" />
+                  <CaixaSeletora titulo="imperfeito" />
+                  <CaixaSeletora titulo="perfeito" />
+                  <CaixaSeletora titulo="mais que perfeito" />
+                  <CaixaSeletora titulo="futuro perfeito" />
+                  <CaixaSeletora titulo="futuro" />
+                </AccordionContent>
+              </AccordionItem>
+              <AccordionItem value="msva">
+                <AcordeaoCabecalho titulo="modo subjuntivo voz ativa" />
+                <AccordionContent>
+                  <CaixaSeletora titulo="presente" />
+                  <CaixaSeletora titulo="imperfeito" />
+                  <CaixaSeletora titulo="perfeito" />
+                  <CaixaSeletora titulo="mais que perfeito" />
+                </AccordionContent>
+              </AccordionItem>
+              <AccordionItem value="mivp">
+                <AcordeaoCabecalho titulo="modo indicativo voz passiva" />
+                <AccordionContent>
+                  <CaixaSeletora titulo="presente" />
+                  <CaixaSeletora titulo="imperfeito" />
+                  <CaixaSeletora titulo="perfeito" />
+                  <CaixaSeletora titulo="mais que perfeito" />
+                </AccordionContent>
+              </AccordionItem>
+              <AccordionItem value="msvp">
+                <AcordeaoCabecalho titulo="modo subjuntivo voz passiva" />
+                <AccordionContent>
+                  <CaixaSeletora titulo="presente" />
+                  <CaixaSeletora titulo="imperfeito" />
+                  <CaixaSeletora titulo="perfeito" />
+                  <CaixaSeletora titulo="mais que perfeito" />
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+            <BotaoApp titulo="iniciar exercicio" tipo="interacao" funcao={()=>{console.log("uninplemented")}} />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
