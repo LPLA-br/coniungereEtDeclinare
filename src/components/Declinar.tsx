ce55a915705bbc81f05c6cb986486275f42e12e7
Luiz Paulo de Lima Araújo <pluiz0674@gmail.com>
2024-08-14 20:01:55 -0300

    func (declinacao): faz modificacoes para funcionamento do declinador
    altera componente TextBiInput -> declinar e altera o declinador.
    adiciona arquivo .env na raiz do projeto para servidor em nuvem.
    coloca types em constants. commit intermediario
    
    tarefa B5

--- src/components/Declinar.tsx
+++ src/components/Declinar.tsx
@@ -1,51 +1,127 @@
 /* LPLA-br 21/03/2024 */
-import React from "react";
-import { View, Text } from 'react-native';
+import { useState, useEffect } from "react";
+import { View, Text, Button } from 'react-native';
+import axios from "axios";
 
 import TextBiInput from "./TextBiInput";
+import BotaoApp from "@/src/components/BotaoApp";
 
 import estiloDeclinacoes from "../styles/componentes/declinar";
 
+import Casos from "../constants/Casos";
+
 type LocalProps =
 {
-  ordenacao: "linhares" | "orberg"
+  ordenacao: "linhares" | "orberg",
+  parametroRota: any //string 
 };
 
-/**
- * Macrocomponente contendo todos os campos
- * necessários para o exercício de declinção.
+/** Macrocomponente responsável por: Requisitar ao servidor
+*   o substantivo correto e modalizar os resultados do encerramento
+*   do exercício.
 * */
 export default function Declinar( props: LocalProps )
 {
+  const [ substantivo, setSubstantivo ] = useState<Casos>(); //request axios
+
+  const [ nomS, setNomS ] = useState<string>("");
+  const [ nomP, setNomP ] = useState<string>("");
+  const [ genS, setGenS ] = useState<string>("");
+  const [ genP, setGenP ] = useState<string>("");
+  const [ datS, setDatS ] = useState<string>("");
+  const [ datP, setDatP ] = useState<string>("");
+  const [ accS, setAccS ] = useState<string>("");
+  const [ accP, setAccP ] = useState<string>("");
+  const [ ablS, setAblS ] = useState<string>("");
+  const [ ablP, setAblP ] = useState<string>("");
+  const [ vocS, setVocS ] = useState<string>("");
+  const [ vocP, setVocP ] = useState<string>("");
+
+  const requisitarDeclinacaoCorreta = async () =>
+  {
+      try
+      {
+        const substantivo = await axios.get( encodeURI( `http://127.0.0.1:8080/nome/substantivo?noms=${props.parametroRota.substantivo}`));
+
+        if ( typeof substantivo == "object" )
+        {
+          console.log(substantivo.data);
+          setSubstantivo( substantivo.data );
+        }
+        else throw new Error( "Resposta não é objeto" );
+      }
+      catch ( err )
+      {
+        console.log( "ERRO: ",err );
+      }
+  }
+
+  //em progresso
+  const aferirResultados = ()=>
+  {
+    const entradaUsuario = {
+      nomS: nomS,
+      nomP: nomP,
+      genS: genS,
+      genP: genP,
+      datS: datS,
+      datP: datP,
+      accS: accS,
+      accP: accP,
+      ablS: ablS,
+      ablP: ablP,
+      vocS: vocS,
+      vocP: vocP
+    };
+    let retorno = {};
+
+    for ( const chave in entradaUsuario )
+    {
+      if ( (entradaUsuario[chave]) == (substantivo[chave]))
+      {
+        retorno[chave] = "acertaste";
+      }
+      else
+      {
+        retorno[chave] = "erraste";
+      }
+    }
+    console.log( JSON.stringify(retorno) );
+  }
+
+  useEffect(
+    ()=>
+    {
+      requisitarDeclinacaoCorreta();
+    },
+    []
+  );
+
   const LINHARES = (
     <>
-      <TextBiInput titulo="Nominativo" campoa="singular" campob="plural" />
-      <TextBiInput titulo="Acusativo"  campoa="singular" campob="plural" />
-      <TextBiInput titulo="Dativo"  campoa="singular" campob="plural" />
-      <TextBiInput titulo="Ablativo"  campoa="singular" campob="plural" />
-      <TextBiInput titulo="Genitivo" campoa="singular" campob="plural" />
-      <TextBiInput titulo="Vocativo" campoa="singular" campob="plural" />
+      <TextBiInput titulo="Nominativo" campoa="singular" campob="plural" settera={setNomS} setterb={setNomP} />
+      <TextBiInput titulo="Acusativo"  campoa="singular" campob="plural" settera={setAccS} setterb={setAccP} />
+      <TextBiInput titulo="Dativo"  campoa="singular" campob="plural" settera={setDatS} setterb={setDatP} />
+      <TextBiInput titulo="Ablativo"  campoa="singular" campob="plural" settera={setAblS} setterb={setAblP} />
+      <TextBiInput titulo="Genitivo" campoa="singular" campob="plural" settera={setGenS} setterb={setGenP} />
+      <TextBiInput titulo="Vocativo" campoa="singular" campob="plural" settera={setVocS} setterb={setVocP} />
     </>
   );
 
   const ORBERG = (
     <>
-      <TextBiInput titulo="Nominativo" campoa="singular" campob="plural" />
-      <TextBiInput titulo="Acusativo"  campoa="singular" campob="plural" />
-      <TextBiInput titulo="Genitivo" campoa="singular" campob="plural" />
-      <TextBiInput titulo="Dativo"  campoa="singular" campob="plural" />
-      <TextBiInput titulo="Ablativo"  campoa="singular" campob="plural" />
-      <TextBiInput titulo="Vocativo" campoa="singular" campob="plural" />
+      <TextBiInput titulo="Nominativo" campoa="singular" campob="plural" settera={setNomS} setterb={setNomP} />
+      <TextBiInput titulo="Acusativo"  campoa="singular" campob="plural" settera={setAccS} setterb={setAccP} />
+      <TextBiInput titulo="Genitivo" campoa="singular" campob="plural" settera={setGenS} setterb={setGenP} />
+      <TextBiInput titulo="Dativo"  campoa="singular" campob="plural" settera={setDatS} setterb={setDatP} />
+      <TextBiInput titulo="Ablativo"  campoa="singular" campob="plural" settera={setAblS} setterb={setAblP} />
+      <TextBiInput titulo="Vocativo" campoa="singular" campob="plural" settera={setVocS} setterb={setVocP} />
     </>
   );
 
   return (
     <View>
-      <Text> DECLINARE </Text>
-      <View>
-
         <View>
-
           <View style={estiloDeclinacoes.singPlur}>
             <Text> SINGULAR </Text>
             <Text> PLURAL </Text>
@@ -53,14 +129,14 @@ export default function Declinar( props: LocalProps )
 
           <View>
             { (typeof props.ordenacao != "undefined") ?
-              ( (props.ordenacao == "linhares") ? LINHARES : ORBERG )
-            :
+              ( (props.ordenacao == "linhares") ? LINHARES : ORBERG ) :
               (<Text> FALHA: propriedade props.ordenacao: string não fornecida. </Text>)
             }
           </View>
-        </View>
 
-      </View>
+          <BotaoApp titulo="aferir resultados" tipo="navegacao" rumo={`/`}></BotaoApp>
+          <Button title="AFERIR RESULTADOS" onPress={()=>{ aferirResultados() }}></Button>
+        </View>
     </View>
   );
 }
--- src/components/TextBiInput.tsx
+++ src/components/TextBiInput.tsx
@@ -1,5 +1,4 @@
 /* LPLA-br 21/03/2024 */
-import React from "react";
 import { View, TextInput, Text } from "react-native";
 
 import estiloTextBiInput from "../styles/componentes/textBiInput";
@@ -8,23 +7,36 @@ type LocalProps =
 {
   titulo: string,
   campoa: string,
-  campob: string
+  campob: string,
+  settera: Function,
+  setterb: Function
 };
 
 /**
- * Sub-componente que representa um título em
- * maiúsculo com dois campos de texto, um ao lado
- * do outro, abaixo do título. requer campo "titulo"
- * em suas propriedades.
+ * Componente de dois inputs textuais que modificam
+ * duas variaveis useState<string>()
+ * no elemento superior.
  * */
 export default function TextBiInput( props: LocalProps )
 {
+
   return (
     <View>
       <Text> {props.titulo} </Text>
       <View id={props.titulo} style={estiloTextBiInput.inputs}>
-        <TextInput id={props.campoa} placeholder={props.campoa} ></TextInput>
-        <TextInput id={props.campob} placeholder={props.campob}></TextInput>
+
+        <TextInput
+          id={props.campoa}
+          placeholder={props.campoa}
+          onEndEditing={ novoTexto => props.settera( novoTexto ) }
+        ></TextInput>
+
+        <TextInput
+          id={props.campob}
+          placeholder={props.campob}
+          onEndEditing={ novoTexto => props.setterb( novoTexto ) }
+        ></TextInput>
+
       </View>
     </View>
   );
